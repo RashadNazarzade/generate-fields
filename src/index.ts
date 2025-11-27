2bcc9c2f0d3972eb31a926393218978775f14159
RashadNazarzade <resadnazar.zade@gmail.com>
2025-11-27 10:09:32 +0400

    feat: adding const for type generics to show fields name

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import type { DICT, GenerateFields } from './types.ts';
 
 import { convert } from './utils/convert.js';
 
-export const generateFields = <Fields extends DICT>(
+export const generateFields = <const Fields extends DICT>(
   fields: Fields,
 ): GenerateFields<Fields> => convert(fields) as GenerateFields<Fields>;
 
